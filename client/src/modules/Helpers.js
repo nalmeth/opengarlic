9d74c22333a2828dd7bd85a4c46b3a39e7937681
nalmeth <nalmeth.tv@gmail.com>
2023-05-21 12:02:12 -0500

    Increased max brush size. Moved inside shape calculations into helpers for easier reading. Reduced default redis expire time to 3hrs

--- client/src/modules/Helpers.js
+++ client/src/modules/Helpers.js
@@ -59,3 +59,103 @@ export const debounce = (fn, ms) => {
 		timer = setTimeout(() => fn.apply(self, args), ms);
 	}
 }
+
+/**
+	 * Returns true if the click event was within the bounds of a
+	 * square (NOT including the stroke)
+	 * @param {object} event
+	 * @param {object} shape
+	 * @param {number} scale stage scale
+	 * @returns {boolean}
+	 */
+export const isInsideRect = (event, shape, scale) => {
+	const points = shape.points;
+
+	// 1/2 the stroke width overlaps the square, so we
+	// Use a 'virtual' smaller square to detect inside the visual box
+	const sw = Math.ceil(shape.strokeWidth/2);
+
+	// top-left and bottom-right x coord
+	const [leftx, rightx] = points[0] < points[2] ?
+			[points[0]+sw, points[2]-sw] : [points[2]+sw, points[0]-sw];
+
+	// top-left and bottom-right y coord
+	const [lefty, righty] = points[1] < points[3] ?
+			[points[1]+sw, points[3]-sw] : [points[3]+sw, points[1]-sw];
+
+	// Account for scale
+	const [scaledLX, scaledLY, scaledRX, scaledRY] =
+			[leftx,lefty,rightx,righty].map(coord => coord * scale);
+
+	// Check if the click was within the bounds
+	return (scaledLX <= event.offsetX && scaledLY <= event.offsetY) &&
+			(scaledRX >= event.offsetX && scaledRY >= event.offsetY);
+}
+
+/**
+	 * Returns true if the click event was within the bounds of a
+	 * square (Including the stroke)
+	 * @param {object} event
+	 * @param {object} shape
+	 * @param {number} scale stage scale
+	 * @returns {boolean}
+	 */
+export const isInsideRectStroke = (event, shape, scale) => {
+	const points = shape.points;
+	// Add stroke to size for comparision to see
+	// if it fits with the stroke included
+	const sw = Math.ceil(shape.strokeWidth/2);
+	// top-left and bottom-right x coord
+	const [leftx, rightx] = points[0] < points[2] ?
+			[points[0]-sw, points[2]+sw] : [points[2]-sw, points[0]+sw];
+
+	// top-left and bottom-right y coord
+	const [lefty, righty] = points[1] < points[3] ?
+			[points[1]-sw, points[3]+sw] : [points[3]-sw, points[1]+sw];
+
+			// Account for scale
+	const [scaledLX, scaledLY, scaledRX, scaledRY] =
+			[leftx,lefty,rightx,righty].map(coord => coord * scale);
+
+	// Check if the click was within the bounds
+	return (scaledLX <= event.offsetX && scaledLY <= event.offsetY) &&
+			(scaledRX >= event.offsetX && scaledRY >= event.offsetY);
+}
+
+/**
+	 * Return true if the click event was withing the bounds of a
+	 * circle (NOT including the stroke)
+	 * @param {object} event
+	 * @param {object} shape
+	 * @param {number} scale stage scale
+	 * @returns {boolean}
+	 */
+export const isInsideCircle = (event, shape, scale) => {
+	const sw = Math.ceil(shape.strokeWidth/2);
+	const cx = shape.points[0] * scale;
+	const cy = shape.points[1] * scale;
+	const r = (shape.radius-sw) * scale;
+	const x = event.offsetX;
+	const y = event.offsetY;
+	// Calculates if a point is inside a circle given the circle center point and radius
+	return (x - cx) * (x - cx) + (y - cy) * (y - cy) <= r * r;
+}
+
+/**
+	 * Returns true if the click event was within the bounds of a
+	 * circle (Including the stroke)
+	 * @param {object} event
+	 * @param {object} shape
+	 * @param {number} scale stage scale
+	 * @returns {boolean}
+	 */
+export const isInsideCircleStroke = (event, shape, scale) => {
+	const sw = Math.ceil(shape.strokeWidth/2);
+	const cx = shape.points[0] * scale;
+	const cy = shape.points[1] * scale;
+	const r = (shape.radius+sw) * scale;
+	const x = event.offsetX;
+	const y = event.offsetY;
+	// Calculates if a point is inside a circle given the circle center point and radius
+	return (x - cx) * (x - cx) + (y - cy) * (y - cy) <= r * r;
+}
